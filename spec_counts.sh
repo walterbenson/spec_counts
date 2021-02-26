ac14c1072912492e33a095a6a9aec7bdab52b504
Walter Benson <walter.benson@sportngin.com>
2021-02-26 16:09:26 -0600

    use sheets query language date format

--- spec_counts.sh
+++ spec_counts.sh
@@ -16,7 +16,7 @@ do
 
   RUNNING=$(echo $OUTPUT | cut -d ' ' -f 1)
   SKIPPED=$(echo $OUTPUT | cut -d ' ' -f 5)
-  NOW=$(date +'%m/%d/%Y')
+  NOW=$(date +'%Y-%m-%d')
   cd ../spec_counts
   printf "$REPO, $RUNNING, $SKIPPED, $NOW\n" >> $FILE_NAME
 done
