f876793ce09c5c0c11fd2cbbdf3dd3b3e15b9c18
Walter Benson <walter.benson@sportngin.com>
2021-02-26 15:22:07 -0600

    finish spec count script

---
+++ spec_counts.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+FILE_NAME=spec_counts_log.csv
+cd ../spec_counts
+printf "repo, running, skipped, date\n" > $FILE_NAME
+
+REPOS=( selenium-finance api-tests contract-api-tests selenium-tests selenium-pro-reg fintech-onboarding-ui-tests )
+for REPO in "${REPOS[@]}"
+do
+  cd ../$REPO
+  pwd
+  git checkout main
+  git pull origin main
+  bundle install
+  OUTPUT=$(bundle exec rspec --dry-run --no-color | grep 'examples, 0 failures')
+
+  RUNNING=$(echo $OUTPUT | cut -d ' ' -f 1)
+  SKIPPED=$(echo $OUTPUT | cut -d ' ' -f 5)
+  NOW=$(date +'%m/%d/%Y')
+  cd ../spec_counts
+  printf "$REPO, $RUNNING, $SKIPPED, $NOW\n" >> $FILE_NAME
+done
